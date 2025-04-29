d6f5856f3cbe63de0af2a7f06c2dcb9e2080f81d
Sayan Mallick <malsayan6@gmail.com>
2025-04-29 19:33:54 +0530

    tenth commit

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,12 +1,15 @@
+// src/app/questions/[quesId]/[quesName]/edit/page.tsx
+
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/sever/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
-
-    return <EditQues question={question} />;
-};
-
-export default Page;
+export default async function EditPage({
+  params,
+}: {
+  params: { quesId: string; quesName: string };
+}) {
+  const question = await databases.getDocument(db, questionCollection, params.quesId);
+  return <EditQues question={question} />;
+}
